5a0f3924c2a5259219565021c4d02343ad9c7f4f
lyh <liaoyinghai@yymt.com>
2025-02-27 09:51:34 +0800

    submit !

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,6 @@ pub fn test_func(_env: JNIEnv, _class: JClass){
     println!("register_native_methods test_func")
 }
 
-// #[unsafe(no_mangle)]
-// pub unsafe extern "C" fn JNI_Onload(_env: JNIEnv, _class: jni::objects::JObject){
-//     let fn_ptr = test_func;
-//     let nmd: jni::NativeMethod = jni::NativeMethod{
-//         name: JNIString::from("test_func"),
-//         sig: JNIString::from("Ljava/lang/Void;"),
-//         fn_ptr: fn_ptr as *mut c_void
-//     };
-//     JNIEnv::register_native_methods(&_env, _class, &[nmd]).expect("register_native_methods");
-// }
-
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn JNI_Onload<'local>(mut _env: JNIEnv<'local>,  _class: JClass<'local>){
     let fn_ptr = test_func;
